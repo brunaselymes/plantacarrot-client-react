65bb944ea29a8f03977962383158b219391824be
AzureAD\BrunaSergio <bruna.sergio@mslsolutions.com>
2021-10-30 05:56:03 +1000

    nav style

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './Nav.css'
 
 const Nav = () => {
+
+    const navSlide = () => {
+
+    }
+
     return (
         <nav>
-            <Link to="/">Home</Link>
+            <div className="logo">
+                <Link className="logo" to="/">Plant a Carrot 🥕</Link>
+            </div>
+            <div className="nav-links">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/">About</Link>
+                <Link className="nav-link" to="/login">Login</Link>
+                <Link className="nav-link" to="/login">Register</Link>
+            </div>
+            <div className="burger" onClick={navSlide}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+
+
+
         </nav>
     );
 }
