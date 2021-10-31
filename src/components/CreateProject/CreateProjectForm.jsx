3afec1749ad3e43bfc6b598c2b79eb009d41d9d9
AzureAD\BrunaSergio <bruna.sergio@mslsolutions.com>
2021-10-31 14:12:31 +1000

    fix style crete project list and add categories dropdown

--- src/components/CreateProject/CreateProjectForm.jsx
+++ src/components/CreateProject/CreateProjectForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './CreateProjectForm.css'
 
 function ProjectForm() {
@@ -33,44 +33,60 @@ function ProjectForm() {
                 })
         }
     };
+
+    const [categoriesList, setCategoriesList] = useState([]);
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}categories/`)
+            .then((results) => {
+                return results.json();
+            })
+            .then((data) => {
+                setCategoriesList(data);
+            });
+    }, []);
+
+    let categoryOption = categoriesList.map((category) =>
+        <option key={category.name}>{category.name}</option>
+    );
+
     return (
         <div className="create-page">
             <div className="create-form-main">
-                <p className="" align="center">Create a project</p>
-                <form onSubmit={handleSubmit} className="form1">
-                    <div>
+                <p className="pagetitle--project" align="center">Create a project</p>
+                <form onSubmit={handleSubmit} className="form2">
+                    <div className="input-wrapper--createproject">
 
-                        <input className="" type="text" id="title" placeholder="Enter your project title" onChange={handleChange} />
-                    </div>
-                    <div>
 
-                        <input className="" type="text" id="image" placeholder="Enter your project image URL" onChange={handleChange} />
-                    </div>
-                    <div>
+                        <input className="field--createproject" type="text" id="title" placeholder="What is the title of your project?" onChange={handleChange} />
 
-                        <input className="" type="text" id="goal" placeholder="What is your goal? $ " onChange={handleChange} />
-                    </div>
-                    <div>
+                        <div>
 
-                        <input className="" type="date" id="close_date" placeholder="When does the project close?" onChange={handleChange} />
-                    </div>
-                    <div>
+                            <input className="field--createproject" type="text" id="image" placeholder="Enter your project image URL" onChange={handleChange} />
+                        </div>
+                        <div>
 
-                        <textarea className="" id="description" placeholder="Entera description for your project" onChange={handleChange} ></textarea>
-                    </div>
-                    <div>
+                            <input className="field--createproject" type="text" id="goal" placeholder="What is your project goal $ ?" onChange={handleChange} />
+                        </div>
+                        <div>
 
-                        <input className="" type="date" id="created_date" placeholder="" onChange={handleChange} />
-                    </div>
-                    <div>
+                            <input className="field--createproject" type="date" id="close_date" placeholder="When does the project close?" onChange={handleChange} />
+                        </div>
+                        <div>
 
-                        <input className="" type="text" id="user" placeholder="Owner of this project" onChange={handleChange} />
-                    </div>
-                    <div>
+                            <textarea className="field--createproject" rows="5" id="description" placeholder="Enter a description for your project" onChange={handleChange} ></textarea>
+                        </div>
+                        <div>
 
-                        <input className="" type="text" id="category" placeholder="category" onChange={handleChange} />
+                            <input className="field--createproject" type="date" id="created_date" placeholder="" onChange={handleChange} />
+                        </div>
+                        <div>
+                            <select className="field--createproject" id="category" placeholder="category">
+                                {categoryOption}
+                            </select>
+                            {/* <input className="field--createproject" type="text" id="category" placeholder="category" onChange={handleChange} /> */}
+                        </div>
+                        <button className="submit--createproject" align="center" type="submit" >Create your project</button>
                     </div>
-                    <button className="" align="center" type="submit" >Create your project</button>
                 </form>
             </div >
         </div>
